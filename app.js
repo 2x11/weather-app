79bf4c0121e6ff0f4cfeae95fc13e7c8e1117ff8
2x11 <lune2x11epee@gmail.com>
2021-08-07 04:29:49 -0300

    fetch application

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
 let weather = {
-    "apiKey": "5d2ba83a6a0d4638fcb763058b23f884"
-}
+    "apiKey": "5d2ba83a6a0d4638fcb763058b23f884",
+    fetchWeather: function (city) {
+        fetch("https://api.openweathermap.org/data/2.5/weather?q="
+        + city 
+        +"&units=metric&lang=es&appid="
+        + this.apiKey
+        )
+        .then((response) => response.json())
+        .then((data) => console.log(data));
+    },
+    displayWeather: function (data) {
+        
+    }
+};
